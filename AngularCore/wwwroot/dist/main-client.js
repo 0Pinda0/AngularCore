0640ad80fa254b226b27e84dc59dbdb5d1e25782
0Pinda0@gmail.com <0pinda0@gmail.com>
2018-05-16 09:45:34 +0100

    - Remove testing suite, will reintroduce later

--- AngularCore/wwwroot/dist/main-client.js
+++ AngularCore/wwwroot/dist/main-client.js
@@ -59,7 +59,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "edbcd47b15bc8ae2a605"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "c4403260eedb121e1f96"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
